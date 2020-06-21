49e1c13f9c9f0be83760e7c1563abc09cc570fe5
Giannis Poulis <ioanniswd@gmail.com>
2020-06-21 23:51:48 +0300

    Show users with no prs in list

--- lib/pr_user_collection.rb
+++ lib/pr_user_collection.rb
@@ -1,14 +1,23 @@
 require_relative 'pr_user'
 
 class PrUserCollection
-  def self.pr_users(user_pr_counts:, options:, actionables_per_author:, untouched_per_author:)
-    @pr_users ||= user_pr_counts.map do |user, counts|
-      PrUser.new(
-        user: user,
-        user_pr_counts: counts,
-        actionable_count: actionables_per_author[user],
-        untouched_count: untouched_per_author[user]
-      )
-    end
+  def self.pr_users(user_pr_counts:, options:, actionables_per_author:, untouched_per_author:, users_to_include:)
+
+    @pr_users ||=
+      user_pr_counts.map do |user, counts|
+        PrUser.new(
+          user: user,
+          user_pr_counts: counts,
+          actionable_count: actionables_per_author[user],
+          untouched_count: untouched_per_author[user]
+        )
+      end +
+      (users_to_include - user_pr_counts.keys).map do |user|
+        PrUser.new(
+          user: user,
+          actionable_count: actionables_per_author[user],
+          untouched_count: untouched_per_author[user]
+        )
+      end
   end
 end
